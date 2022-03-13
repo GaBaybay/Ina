c39d1a718f758350597d3e8badd7f6452deacb1d
GaBaybay <100408215+GaBaybay@users.noreply.github.com>
2022-03-13 12:25:18 +0800

    Update main.js

--- main.js
+++ main.js
@@ -294,7 +294,7 @@ login({ appState: JSON.parse(process.env['fbstate'])}, (err, api) => {
           }else if (input.startsWith("?help")) {
             let data = input.split(" ");
                         if (data.length < 2) {
-                            api.sendMessage(" ⚜️Commands List⚜️ \n\n<br>?help--for commands\n?play (title of a song)--for playing a song\n?motivation-- for motivational quotes\n?wiki (word)--for wiki search\n?leech--for direct links in YouTube\n?tiktokdl (TikTok) --for downloading videos from tiktok\nKill: Unsent-- for custom thread\nActivate: Unsent--for custom thread\nDisable: Unsent--for all thread\nEnable: Unsent--for all thread\n\n\nMade By Dev✒️ GaBaybay ✒️", event.threadID, event.messageID);
+                            api.sendMessage(" ⚜️Commands List⚜️ \n\n<br>?help--for commands\n<br>?play (title of a song)--for playing a song\n<br>?motivation-- for motivational quotes\n<br>?wiki (word)--for wiki search\n<br>?leech--for direct links in YouTube\n<br>?tiktokdl (TikTok) --for downloading videos from tiktok\n<br>Kill: Unsent-- for custom thread\n<br>Activate: Unsent--for custom thread\n<br>Disable: Unsent--for all thread\n<br>Enable: Unsent--for all thread\n\n\n<br>Made By Dev✒️ GaBaybay ✒️", event.threadID, event.messageID);
                         }
           }else if(input.startsWith("?wiki")) {
             let data = input.split(" ");
